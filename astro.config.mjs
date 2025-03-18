514df6fc755ce2f0f7594bbc0cd02e7a8624c2c4
Alex-PC <alex03marcos@gmail.com>
2025-03-18 18:33:02 +0100

    Fix dependencies and cloudflare connection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
-  adapter: vercel(),
+  adapter: cloudflare()
 });
